0743445b48252a6f07380c0ecbbe87f587be8840
DaequanWSCW <sud@wsc.school.nz>
2024-05-02 10:30:39 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# Ask the user their name
+name = input("What is your name?")
+# Greet the user and introduce the quiz
+print("Welcome to this quiz,", name)
+print("This quiz is on Maths")
+# Ask the user a question
+answer = input("what is 1+1?")
+# Tell the user the answer
+print("The Answer is 234!")
