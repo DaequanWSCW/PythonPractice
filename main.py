4da6df49f988a4737a5ea6d6776a88c2cbe093f4
DaequanWSCW <sud@wsc.school.nz>
2024-05-06 11:58:16 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
 # Ask the user their name
-name = input("What is your name?")
+name = input("What is your name? ")
 # Greet the user and introduce the quiz
 print("Welcome to this quiz,", name)
 print("This quiz is on Maths")
 # Ask the user a question
-answer = input("what is 1+1?")
-# Tell the user the answer
-print("The Answer is 234!")
+answer = input("what is 1+1? ")
+if answer == "234" :
+    print("Correct!")
+else:
+    print("INCORECT HAHAH")
+print("The Answer is 234")
+
+
