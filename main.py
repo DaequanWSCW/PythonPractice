baea0729dc00510f3c7b08bd88f0a947a8f9225c
DaequanWSCW <sud@wsc.school.nz>
2024-05-07 14:27:17 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+score = 0
 # Ask the user their name
 name = input("What is your name? ")
 # Greet the user and introduce the quiz
@@ -8,8 +8,16 @@ print("This quiz is on Maths")
 answer = input("what is 1+1? ")
 if answer == "234" :
     print("Correct!")
+ 
 else:
     print("INCORECT HAHAH")
 print("The Answer is 234")
+# Ask the user another question
+answer = input("What is 2+239857?")
+if answer == "239847" :
+    print("Correct")
+else: 
+    print ("INCORRECT YOU ARE TERRIBLEE!!!")
+
 
 
