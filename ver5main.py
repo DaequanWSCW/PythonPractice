7e5907f936652ee6268982b170c72ae10aea192c
DaequanWSCW <sud@wsc.school.nz>
2024-05-27 11:58:59 +1200

    started ver 5

--- ver5main.py
+++ ver5main.py
@@ -1,11 +1,22 @@
 import random
 
-
+QUESTION_FORMAT = "{}\nA.{} B.{} C.{} D.{}"
+GOOD_COMMENTS = ["Not bad!", "Good job skibidi","The City needs you Batman"]
+BAD_COMMENTS = ["You suck", "Why are you still here? Quit already.", "Get out of my quiz, you are terrible"]
+QUESTIONS = ["What is Batmans real name?",
+             "Who is the creator of this quiz?",
+             "What is the capital of Australia?"]
+OPTIONS =[["Peter Parker", "Medhansh Anthwal", "Bruce Wayne", "Tony Stank"],
+          ["Daequan Su", "Medhansh Anthwal", "Floyd Morgan", "Oscar White"],
+          ["Sydney", "Melbourne", "Perth", "Canberra"]]
+SHORT_OPTIONS = ["a", "b", "c", "d"]
+ANSWERS = [3,1,4]
 
 
 score = 0
 play = "yes"
 # Ask the user their name
+
 name = input("What is your name? ")
 if name == "Daequan":
     print("I LOVE YOU MY ALMIGHTY SIGMA LORD YOU ARE SO SKIBIDY")
@@ -28,179 +39,95 @@ while True:
 while play == "yes":
     score = 0
 
+  
         
-
-    question_attempts= tries
-    while question_attempts > 0:
+    for i in range(len(QUESTIONS)):
+        question_attempts= tries
+        while question_attempts > 0:
         
-        if name =="Daequan":
-            score = 100000
-        else:
-            score = 0
-        answer = input("What is Batmans Real name?").upper().lower()
-        # Congratulates them for getting the correct answer
-        if answer == "Bruce Wayne" .upper().lower():
-            print("Correct!")
-            score +=5
+        
+           answer = input(QUESTION_FORMAT.format(QUESTIONS[i], OPTIONS[i][0],
+                                                 OPTIONS[i][1], OPTIONS[i][2], OPTIONS[i][3])).lower()
+        if answer == c or answer == "c" :
+            print("Correct")
+            score+=5
             print("You have been awarded 5 points")
             break
-            # Questions the user (If they left the question blank)
         elif answer == "":
             print("Not anwering cuh?")
             print ("You have been awarded 0 points")
-            # Tells the user that they are INCORRECT LOLLL
+        elif answer !=a and answer != "a" and answer !=b and answer != "b" and answer !=c and answer != "c" and answer !=d and answer != "d":
+            print("That isn't a option")
+            print("You have been awarded 5 points")
         else:
-            print("INCORECT HAHAH")
-        
-            print ("You have been awarded 0 points")
+            print("INCORRECT YOU ARE TERRIBLE")
+            print("You have been awarded 0 points")
 
             question_attempts -=1
-    print("The Answer is Bruce Wayne")
+    print("The Answer is {}".format(c))
 
    
 
     question_attempts= tries
     while question_attempts > 0:
-        answer = input("What is the name of the creator of this quiz?").upper().lower()
-        # Congratulates them for getting the correct answer
-        if answer == "Daequan".upper().lower() :
-            print("Correct")
-            score+=5
-            print("You have been awarded 5 points")
-            break
-        # Questions the user (If they left the question blank)
-        elif answer == "":
-            print("Not anwering cuh?")
-            print ("You have been awarded 0 points")
-        # Tells the user that they are INCORRECT LOLLL
-        else: 
-            print ("INCORRECT YOU ARE TERRIBLEE!!!")
-            
-            print ("You have been awarded 0 points")
-            question_attempts -=1
-    print("The answer was Daequan")
-
-
-    question_attempts= tries
-    while question_attempts > 0:
-        answer = input ("What is the Capital of Australia?").upper().lower()
-        # Congratulates them for getting the correct answer
-        if answer == "Canberra".upper().lower() :
-            print("Correct")
-            score+=5
-            print("You have been awarded 5 points")
-            break
-        # Questions the user (If they left the question blank)
-        elif answer == "":
-            print("Not anwering cuh?")
-            print ("You have been awarded 0 points")
-        # Tells the user that they are INCORRECT LOLLL
-        else: 
-            print ("INCORRECT YOU ARE TERRIBLEE!!!")
-            print ("You have been awarded 0 points")
-            question_attempts -=1
-    print("The answer is Canberra")
-
-
-    question_attempts= tries
-    while question_attempts > 0:
-        question = "How many stripes are in the USA Flag?"
-        a = "8"
-        b = "16"
-        c = "13"
-        d = "10"
+        question = "Who is the creator of this quiz?"
+        a = "Daequan Su"
+        b = "Medhansh Anthwal"
+        c = "Floyd Morgan"
+        d = "Oscar White"
         # Tells user they are correct
-        answer = input("{}\nA.{} B.{} C.{} D.{}".format(question, a, b, c, d)).upper().lower()
-        if answer == c or answer == "c" :
-            print("Correct")
-            score+=5
-            print("You have been awarded 5 points")
-            break
-        elif answer == "":
-            print("Not anwering cuh?")
-            print ("You have been awarded 0 points")
-        elif answer !=a and answer != "a" and answer !=b and answer != "b" and answer !=c and answer != "c" and answer !=d and answer != "d":
-            print("That isn't a option")
-            print("You have been awarded 5 points")
-        else:
-            print("INCORRECT YOU ARE TERRIBLE")
-            print("You have been awarded 0 points")
-            question_attempts -=1
-    print("The answer is {}".format (c))
-    
-    input("We are halfway and you currently have {} points press enter when ready".format (score))
-
-
-
-    question_attempts= tries
-    while question_attempts > 0:
-        # Tells the user the next question
-        question = "What is the biggest city by Land??"
-        a = "Tokyo"
-        b = "Los Angeles"
-        c = "New York"
-        d = "Auckland"
-        # Tell user they are correct
-        answer = input("{}\nA.{} B.{} C.{} D.{}".format(question, a, b, c, d)).upper().lower()
-        if answer == c or answer == "c" :
-            print("Correct")
-            score+=5
-            
-            print("You have been awarded 5 points")
-            break
-            # Questions the user
-        elif answer == "":
-            print("Not anwering cuh?")
-            
-            print ("You have been awarded 0 points")
-        elif answer !=a and answer != "a" and answer !=b and answer != "b" and answer !=c and answer != "c" and answer !=d and answer != "d":
-            print("That isn't a option")
-            
-            print("You have been awarded 5 points")
-        # Tells the user they are wrong
-        else:
-            print("INCORRECT YOU ARE TERRIBLE")
-            print("You have been awarded 0 points")
-            question_attempts -=1
-    print("The answer is {}".format (c))
-
-    question_attempts= tries
-    while question_attempts > 0:
-        # Tells the user the next question
-        question = "What is the biggest city by POPULATION??"
-        a = "Tokyo"
-        b = "Shanghai"
-        c = "New York"
-        d = "Wellington"
-        # Tell user they are correct
         answer = input("{}\nA.{} B.{} C.{} D.{}".format(question, a, b, c, d)).upper().lower()
         if answer == a or answer == "a" :
             print("Correct")
             score+=5
-            
             print("You have been awarded 5 points")
             break
-            # Questions the user
         elif answer == "":
             print("Not anwering cuh?")
-            
             print ("You have been awarded 0 points")
         elif answer !=a and answer != "a" and answer !=b and answer != "b" and answer !=c and answer != "c" and answer !=d and answer != "d":
             print("That isn't a option")
-            
             print("You have been awarded 5 points")
-        # Tells the user they are wrong
         else:
             print("INCORRECT YOU ARE TERRIBLE")
             print("You have been awarded 0 points")
             question_attempts -=1
-    print("The answer is {}".format (a))
+    print("The answer was {}".format (a))
+
+
+    question_attempts= tries
+    while question_attempts > 0:
+        question = "What is the capital of Australia?"
+        a = "Sydney"
+        b = "Melbourne"
+        c = "Perth"
+        d = "Canberra"
+        # Tells user they are correct
+        answer = input("{}\nA.{} B.{} C.{} D.{}".format(question, a, b, c, d)).upper().lower()
+        if answer == d or answer == "d" :
+            print("Correct")
+            score+=5
+            print("You have been awarded 5 points")
+            break
+        elif answer == "":
+            print("Not anwering cuh?")
+            print ("You have been awarded 0 points")
+        elif answer !=a and answer != "a" and answer !=b and answer != "b" and answer !=c and answer != "c" and answer !=d and answer != "d":
+            print("That isn't a option")
+            print("You have been awarded 5 points")
+        else:
+            print("INCORRECT YOU ARE TERRIBLE")
+            print("You have been awarded 0 points")
+            question_attempts -=1
+    print("The answer was {}".format (d))
+
+
     
     
     
     #Tells the user it is the end of the quiz
     print("You have completed the quiz!")
     print("Congratulations, {}".format (name))
-    print("You have gotten a score of {} out of 30".format (score))
+    print("You have gotten a score of {} out of 15".format (score))
 
     play = input("Do you want to try this quiz again?").lower()
